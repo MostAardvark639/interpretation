93d1ee9f05bc14a86322393982f2bf0534575de7
MostAardvark <54926720+MostAardvark639@users.noreply.github.com>
2019-10-01 08:51:24 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,82 @@
+function setup() {
+
+
+	createCanvas(610, 500)
+	fill(0,0,0)
+	rect (0, 0, 610, 500)
+	stroke(0,0,0)
+	strokeWeight(5)
+	fill(255)
+	//top left box
+	rect(15,15, 100, 75)
+	//second on top row
+	rect(130, 15, 75, 75)
+	//line(10,10, 50, 50)
+    // third on top row
+	rect(220, 15, 120, 75)
+	//fourth on top row
+	rect(355, 15, 150, 75)
+	//5th on top
+	rect(520, 15, 75, 75)
+
+
+	//second row 1st box
+	rect(15, 110, 100, 115)
+	//second row 2nd box (red box)
+	fill(255,0,0)
+	rect(130, 110, 75, 115)
+
+	//3rd on 2nd row
+	fill(255)
+	rect(220, 110, 120, 115)
+
+	//4th on 2nd row
+	fill(0,0,255)
+	rect(355, 110, 150, 115)
+
+	//5th on 2nd row
+	fill(255)
+	rect(520, 110, 75, 115)
+
+	//1st on 3rd row
+	rect(15, 240, 100, 130)
+
+	//2nd on 3rd row top white part
+	rect(130, 240, 75, 80)
+	//2nd on 3rd row bottom yellow
+	fill(243,252,0)
+	rect(130, 328, 75, 40)
+
+	//3rd on 3rd row 
+	fill(255)
+	rect(220, 240, 120, 130)
+
+	//4th on 3rd row
+	rect(355, 240, 150, 130)
+
+	//5th on 3rd row
+	rect(520, 240, 75, 130)
+
+	//1st on 4th row
+	rect(15, 385, 100, 100)
+
+	//2nd on 4th row
+	rect(130, 385, 75, 100)
+
+	//3rd on 4th row
+	rect(220, 385, 120, 100)
+
+	//4th on 4th row
+	rect(355, 385, 150, 100)
+
+	//5th on 4th row
+	rect(520, 385, 75, 100)
+
+//fill(0,0,0)
+//noStroke()
+//line(0, 0, 100, 300)
+//line(0, 300, 400, 0)
+
+
+
+}
